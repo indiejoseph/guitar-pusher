261eb404b00da10690395507f24579285a028ad4
indiejoseph@gmail.com <indiejoseph@gmail.com>
2026-04-24 11:18:53 +0100

    fix: update base path in Vite configuration to root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/guitar-pusher/",
+	base: "/",
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
